d491df0d76131fbab8c100286831cf4549efae99
CHONG MING ZHE <25005796@siswa.um.edu.my>
2025-12-01 11:45:42 +0800

    First commit

---
+++ netbeans/lab/src/lab/Lab.java
@@ -0,0 +1,14 @@
+
+package lab;
+
+public class Lab {
+
+    public static void main(String[] args) {
+      String []student={"noor","adam"};
+      for (String i:student)
+         System.out.println(i);
+      
+      
+    }
+    
+}
